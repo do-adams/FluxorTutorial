fb1851596b7f404143ed8404010d6f0e3bf52647
Damian <contact@mianlabs.com>
2020-09-25 14:04:12 -0500

    First Blazor tutorial: state, actions, and reducers

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using FluxorTutorial.Store.CounterUseCase;
+
+namespace FluxorTutorial.Pages
+{
+    public partial class Counter
+    {
+        [Inject]
+        private IState<CounterState> CounterState { get; set; }
+
+        [Inject]
+        public IDispatcher Dispatcher { get; set; }
+
+        private void IncrementCount()
+        {
+            var action = new IncrementCounterAction();
+            Dispatcher.Dispatch(action);
+        }
+    }
+}
